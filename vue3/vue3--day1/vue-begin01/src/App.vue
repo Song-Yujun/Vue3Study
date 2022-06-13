6284de1c8484966b551d943aa9420dd577b29bc6
songyujun <1061843665@qq.com>
2022-06-14 01:09:02 +0800

    watch深度监听，class内联样式

--- vue3/vue3--day1/vue-begin01/src/App.vue
+++ vue3/vue3--day1/vue-begin01/src/App.vue
@@ -2,24 +2,45 @@
 export default {
   data() {
     return {
-      message:"helloWorld",
-      age:0
+      message: "helloWorld",
+      isActive: true,
+      error:10,
+      classObj: {
+        active: true,
+        helloWorld: true
+      },
+      activeClass:"active"
     }
   },
-  methods: {
-
-  },
   computed:{
-
-  },
-  watch:{
-
+    classObjCom:function(){
+      return {
+        active:this.isActive && !this.error,
+        helloWorld:this.error
+      }
+    }
   }
 }
 </script>
 
 <template>
- 
+  <!-- 第一种：放置字符串 -->
+  <p class="active">hello</p>
+  <!-- 第二种：放置对象 -->
+  <!-- <p :class="{类名:boolean}">hello</p> -->
+  <p :class="{ active: isActive }">hello</p>
+  <p :class="{ active: isActive, helloWorld: isActive }">hello</p>
+  <button @click="isActive = !isActive">改变active展示</button>
+  <!-- 和普通类同时存在，不会起冲突 -->
+  <p :class="{ active: isActive }" class="helloWorld">hello</p>
+  <p :class="classObj">hello</p>
+  <!-- 使用computed -->
+  <p :class="classObjCom">hellocomputed</p>
+
+  <!-- 第三种：数组语法（不常用） -->
+  <p :class="[message,activeClass]">helloArray</p>
+  <!-- 数组和对象结合使用 -->
+  <p :class="[message,{active:isActive}]">hello3</p>
 </template>
 
 <style>
@@ -32,15 +53,12 @@ export default {
   margin-top: 60px;
 }
 
-#d1 {
-  color: red;
-}
-
-.d1 {
+.active {
   font-size: 50px;
+  color: green;
 }
 
-#d2 {
-  color: blue;
+.helloWorld {
+  background-color: pink;
 }
 </style>
---
+++ vue3/vue3--day1/vue-begin01/src/监听器.vue
@@ -0,0 +1,97 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "你好",
+      age: 0,
+      user:{
+        name:"张三",
+        age:18,
+        sex:"男"
+      }
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  watch: {// 监听数据变化
+    // 每当message发生变化的时候，该函数就会被调用
+    // message:function(newValue,oldValue){
+    //   // 执行异步操作，或者复杂的逻辑代码
+    //   console.log(newValue,"新值");
+    //   console.log(oldValue,"旧值");
+    //   if(newValue.length<5 || newValue.length<10){
+    //     console.log("输入框中的内容长度不能小于5或者不能大于10");
+    //   }
+    // }
+
+    message: {
+      immediate: true,// 初始化的时候调用函数
+      handler: function (newValue) {
+        console.log(newValue);
+        if (newValue.length < 5 || newValue.length < 10) {
+          console.log("输入框中的内容长度不能小于5或者不能大于10");
+        }
+      }
+    },
+
+    // 监听不到对象的属性变化，需要使用深度监听
+    // user:function(newValue){
+    //   console.log(newValue);
+    // }
+
+
+    // user:{
+    //   handler:function(newValue){
+    //     console.log(newValue);
+    //   },
+    //   deep:true
+    // }
+
+     "user.name":{ //使用字符串的形式进行优化，只会单独监听对象中对应的属性
+      handler:function(newValue){
+        console.log(newValue);
+      },
+      deep:true
+    }
+
+  }
+}
+</script>
+
+<template>
+  <p>{{ message }}</p>
+  <button @click="message = 'hello'">改变message</button>
+
+  <!-- v-model:数据的双向绑定 -->
+  <input type="text" v-model="message">
+
+  <p>{{user.name}}</p>
+  <button @click="user.name='李四'">改变名字</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#d1 {
+  color: red;
+}
+
+.d1 {
+  font-size: 50px;
+}
+
+#d2 {
+  color: blue;
+}
+</style>
